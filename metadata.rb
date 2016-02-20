62f6c0d719ad89b32e7631ddde2b3d260b89aa45
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-02-20 11:47:27 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'nodejs'
+maintainer 'Jason Blalock'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures nodejs'
+long_description 'Installs/Configures nodejs'
+version '0.1.0'
+
+depends 'apt'
