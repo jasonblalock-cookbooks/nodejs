2e24378a2431f9d0b25730cabe8a8579b0abd676
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-02-20 13:08:35 -0500

    Add serverspecs for nodejs and npm with correct version

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 describe 'nodejs::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package('nodejs') do
+    it { should be_installed }
+  end
+
+  context 'using default attributes' do
+    describe command('nodejs --version') do
+      its(:stdout) { should start_with 'v4.3' }
+    end
+
+    describe command('npm --version') do
+      its (:stdout) { should start_with '2.14' }
+    end
   end
 end
