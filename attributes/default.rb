5b556141e74c9f1b7abeeb691836f667d60573c6
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-05-28 16:41:29 -0400

    Add dynamic distro name to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,14 @@
 # MIT License
 #
 
+distro_name = case node['platform_version']
+when '14.04'
+  'trusty'
+when '16.04'
+  'xenial'
+end
+
 default['nodejs']['version'] = 'node_4.x'
-default['nodejs']['platform']['distro'] = 'trusty'
+default['nodejs']['platform']['distro'] = distro_name
 default['nodejs']['repository']['uri'] = "https://deb.nodesource.com/#{node['nodejs']['version']}"
 default['nodejs']['repository']['key'] = 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
